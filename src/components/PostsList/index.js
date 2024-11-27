23b2d4c9ab5140986ef63d6e795fa8f55ea48edd
Pa1ngZ1nOo <paingzinoo@acedatasystems.com>
2024-11-27 20:54:43 +0630

    Basic Concepts

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./index.css";
+import styles from "./single_post.module.css";
 
 export default function PostsList({ posts }) {
   //   console.log(anotherData);
   return (
-    <div className="postList">
+    <div className="postList container">
       {posts.map((post) => (
-        <div className="single-post">{post.title}</div>
+        <div className={`single-post ${styles.card}`} key={post.id}>
+          {post.title}
+        </div>
       ))}
     </div>
   );
